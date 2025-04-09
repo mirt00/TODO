f90c1b0af166ef250a2d8ea9ae0966bd7adf7805
amrit <bhudathokiamrit@gmail.com>
2025-04-09 20:46:59 +0545

    model and routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-require("./conn/conn");
+const mongoose = require("mongoose")
+
 app.get("/", (req, res) => {
     res.send("hello");
 });
 
-app.listen(1000, () => {
+app.listen(1000, async () => {
+    await mongoose.connect("mongodb+srv://bhudathokiamrit:Nopassword%40123@cluster0.flo6veb.mongodb.net/ToDodb")
+    .then(()=>{
+        console.log("Database Connected");
+    });
     console.log("Server Started");
 });
