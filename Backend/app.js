5167c3befa3903abb1651d450b3577653f84bf55
amrit <bhudathokiamrit@gmail.com>
2025-04-09 21:43:12 +0545

    add user register

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
 
+const auth = require("./routes/auth")
+
 app.get("/", (req, res) => {
     res.send("hello");
 });
+app.use(express.json())
+app.use("/api/v1", auth);
 
-app.listen(1000, async () => {
+app.listen(3000, async () => {
     await mongoose.connect("mongodb+srv://bhudathokiamrit:Nopassword%40123@cluster0.flo6veb.mongodb.net/ToDodb")
     .then(()=>{
         console.log("Database Connected");
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -2,10 +2,12 @@
  const userSchema = new mongoose.Schema({
     email:{
         type:String,
+        unique: true,
         require: true,
     },
     username:{
         type: String,
+        unique: true,
         require: true,
     },
     password:{
