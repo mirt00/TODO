7d3bbfaf1806dbbcd8d84a478d2e4c35a2761ffb
amrit <bhudathokiamrit@gmail.com>
2025-04-12 08:12:54 +0545

    updated env

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config()
+
 const mongoose = require("mongoose")
 const cors = require("cors");
 const auth = require("./routes/auth")
+const mongoUri = process.env.MONGO_URI
 const list = require("./routes/list")
 app.use(cors({
     origin: "http://localhost:5173", // Allow your frontend origin
@@ -16,7 +19,7 @@ app.use("/api/v",list);
 app.use("/api/v1", auth);
 
 app.listen(3000, async () => {
-    await mongoose.connect("mongodb+srv://bhudathokiamrit:Nopassword%40123@cluster0.flo6veb.mongodb.net/ToDodb")
+    await mongoose.connect(mongoUri)
     .then(()=>{
         console.log("Database Connected");
     });
